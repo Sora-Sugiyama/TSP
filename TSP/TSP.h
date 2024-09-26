30c64cca35b7c8a1a144561395cff06922dd2d8b
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2024-09-27 04:10:14 +0900

    Update TSP.h

--- TSP/TSP.h
+++ TSP/TSP.h
@@ -8,7 +8,22 @@
 #ifndef TSP_h
 #define TSP_h
 
+#include <vector>
+#include <random>
+#include <algorithm>
+
 namespace TSP{
+class problem{
+public:
+    std::string WeightType;
+    int dimension;
+    std::vector<std::vector<double> >coord;
+    std::vector<int>optTour;
+    std::vector<std::vector<double> >W;
+};
 }
+
+#include "TSPLIBloader.h"
+#include "greedy.h"
 
 #endif /* TSP_h */
